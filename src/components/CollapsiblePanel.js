88058a18858035713b180a594e9c4991dfb96baa
LarissaMorrell <larissa@larissamorrell.com>
2018-03-28 18:09:56 -0400

    cleanup

--- src/components/CollapsiblePanel.js
+++ src/components/CollapsiblePanel.js
@@ -5,22 +5,12 @@ export default class CollapsiblePanel extends Component {
     super(props);
     this.state = {
       //TODO add arrow for smaller screen size
-      //TODO pass in props different panelContents
-      expandIcon: true,
       showContent: false,
       panelContent: this.props.panelComponent,
       showMoreDetails: false
     };
-    this.addDetails = this.addDetails.bind(this);
   }
   addDetails() {
-    let newState = Object.assign({}, this.state, {
-      expandIcon: !this.state.expandIcon
-    });
-    console.log(this.state.panelContent);
-    this.setState(newState);
-  }
-  showContentToggle() {
     let newState = Object.assign({}, this.state, {
       showContent: !this.state.showContent
     });
@@ -28,42 +18,30 @@ export default class CollapsiblePanel extends Component {
   }
   showMoreDetailsToggle() {
     let newState = Object.assign({}, this.state, {
-      showMore: !this.state.showMoreDetails
+      showMoreDetails: !this.state.showMoreDetails
     });
     this.setState(newState);
   }
 
   render() {
-    let content = () => {
-      if (this.props.showContent) {
-        return (
-          <div
-            className="collapsible-panel-details-container"
-            onClick={() => this.showContentToggle()}
-          >
-            {this.state.expandIcon ? null : this.state.panelContent}
-            <button onClick={() => this.showMoreDetailsToggle()}>
-              {this.state.showMoreDetails ? "Show Less" : "Show More"}
-            </button>
-          </div>
-        );
-      } else {
-        return;
-      }
-    };
+    let showContent = this.state.showContent;
+    let content = (
+      <div className="collapsible-panel-details-container">
+        {showContent ? this.state.panelContent : null}
+        <button onClick={() => this.showMoreDetailsToggle()}>
+          {this.state.showMoreDetails ? "Show Less" : "Show More"}
+        </button>
+      </div>
+    );
 
     return (
       <div className="collapsible-panel">
         <button onClick={() => this.addDetails()}>
           <h3>{this.props.title}</h3>
-          <span className="expand-icon">
-            {this.state.expandIcon ? "+" : "-"}
-          </span>
+          <span className="expand-icon">{showContent ? "-" : "+"}</span>
         </button>
-        {content}
+        {showContent ? content : null}
       </div>
     );
   }
 }
-
-//
