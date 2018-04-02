eb4b8b34f8b50e53d5492b498b8b44f83abc1a51
LarissaMorrell <larissa@larissamorrell.com>
2018-04-02 14:13:07 -0400

    (fixed) language carousel working

--- src/components/LanguagesPanel.js
+++ src/components/LanguagesPanel.js
@@ -4,27 +4,35 @@ import LanguageCard from "./LanguageCard.js";
 
 export default class LanguagesPanel extends Component {
   render() {
-    var settings = {
-      dots: true,
+    let settings = {
       arrows: true,
       infinite: true,
-      slidesToScroll: 1,
+      slidesToShow: 3,
+      slidesToScroll: 2,
       arrows: true
     };
+
     return (
       <Slider {...settings} className="test">
-        <LanguageCard language="React" skill-level={5} />
-        <p className="language-card">yes</p>
-        <LanguageCard language="React" skill-level={5} />
-        <p>hi</p>
-        <p>no</p>
+        <div>
+          <LanguageCard language="React" skill-level={5} />
+        </div>
+        <div>
+          <LanguageCard language="Javascript" skill-level={4} />
+        </div>
+        <div>
+          <LanguageCard language="HTML" skill-level={5} />
+        </div>
+        <div>
+          <LanguageCard language="CSS" skill-level={5} />
+        </div>
+        <div>
+          <LanguageCard language="Node" skill-level={3.5} />
+        </div>
+        <div>
+          <LanguageCard language="Java" skill-level={4} />
+        </div>
       </Slider>
     );
   }
-  // <LanguageCard language="React" skill-level={5} />
-  // <LanguageCard language="Javascript" skill-level={4} />
-  // <LanguageCard language="HTML" skill-level={5} />
-  // <LanguageCard language="CSS" skill-level={5} />
-  // <LanguageCard language="Node" skill-level={3.5} />
-  // <LanguageCard language="Java" skill-level={4} />
 }
