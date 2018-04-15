2034d8a30ec46b11c19abbaa0cb14ac7ae302086
LarissaMorrell <larissa@larissamorrell.com>
2018-04-15 18:22:29 -0400

    (added) about description

--- src/components/AboutLarissaContainer.js
+++ src/components/AboutLarissaContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import CollapsiblePanel from "./CollapsiblePanel.js";
+import AboutDescription from "./AboutDescription.js";
 import AboutSkills from "./AboutSkills.js";
 import AboutEducation from "./AboutEducation.js";
 import AboutExperience from "./AboutExperience.js";
@@ -7,8 +8,12 @@ import AboutExperience from "./AboutExperience.js";
 export default class AboutLarissaContainer extends Component {
   render() {
     return (
-      <div>
+      <div className="about-larissa-container">
         <h2>More About Larissa</h2>
+        <CollapsiblePanel
+          title="Description"
+          panelComponent={<AboutDescription />}
+        />
         <CollapsiblePanel title="Skills" panelComponent={<AboutSkills />} />
         <CollapsiblePanel
           title="Education"
