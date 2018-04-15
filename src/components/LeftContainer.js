dcd5525b0d81c06cb9d2a4d2ec35bb4c658b0499
LarissaMorrell <larissa@larissamorrell.com>
2018-04-15 09:11:35 -0400

    (fixed) sticky active only when window >600

--- src/components/LeftContainer.js
+++ src/components/LeftContainer.js
@@ -4,10 +4,38 @@ import SubNav from "./SubNav.js";
 import CodePreviewContainer from "./CodePreviewContainer.js";
 
 export default class LeftContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: window.innerWidth
+    };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+
+  updateWindowDimensions() {
+    let width = window.innerWidth;
+    this.setState({ width: width });
+  }
+
+  /**
+   * Add event listener
+   */
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener("resize", this.updateWindowDimensions);
+  }
+
+  /**
+   * Remove event listener
+   */
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateWindowDimensions);
+  }
+
   render() {
     return (
       <div className="left-container">
-        <Sticky bottomBoundary={1200}>
+        <Sticky enabled={this.state.width > 600} bottomBoundary={1200}>
           <SubNav />
           <CodePreviewContainer />
         </Sticky>
