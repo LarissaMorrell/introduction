b9b79f95e00e8f8e8cb8e49dc8d940d9176f1f47
LarissaMorrell <larissa@larissamorrell.com>
2018-04-16 13:45:38 -0400

    update images

--- src/components/LanguageCard.js
+++ src/components/LanguageCard.js
@@ -7,7 +7,7 @@ export default class LanguageCard extends Component {
       <div className="card">
         <img
           className="language-logo"
-          src={`./media/${this.props.language.toLowerCase()}-logo.png`}
+          src={`./media/languages/${this.props.language.toLowerCase()}-logo.png`}
           alt={`${this.props.language}`}
         />
         <h3>{this.props.language}</h3>
